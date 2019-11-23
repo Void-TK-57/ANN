750430734f12e330e2da0324e009a5f018fc6e19
Void <pedroaa57@gmail.com>
2019-11-22 21:43:49 -0300

    Add Validation Data Treatment

--- doe.py
+++ doe.py
@@ -41,12 +41,11 @@ def create_model(hyperparameters):
         return model
 
 # plot model
-def plot(history, epochs, ax = plt):
+def plot(model, epochs, ax = plt):
     # x
     x = np.arange(0, epochs)
-    print(history.history.keys())
     # plot accruacy
-    ax.plot(x, history.history["accuracy"], label="train acc")
+    ax.plot(x, model.history["accuracy"], label="train acc")
 
 # save model
 def save(model, file = './model/model.json'):
@@ -85,28 +84,22 @@ def main(args):
     # get values as matrix
     matrix = data.values
     # get y and x values
-    x_data, y_data = matrix[:, :-1].astype(np.float64), matrix[:, -1]
+    x_data, y_data = matrix[:, :-2].astype(np.float64), matrix[:, -1]
     # norazlize x_data
     x_data = normalize(x_data)
-    # data slices
-    training_slice = hyperparameters["training slice"]
-    validation_slice = hyperparameters["validation slice"]
-    test_slice = 1.0 - training_slice - validation_slice
-    # from data get train and rest
-    x_train, x_rest, y_train, y_rest = train_test_split(x_data, y_data, train_size = training_slice)
-    # from rest get validation and test
-    x_validation, x_test, y_validation, y_test = train_test_split(x_rest, y_rest, train_size = validation_slice/(validation_slice + test_slice) )
+    # get train and test
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size = hyperparameters["training slice"])
 
     # encoder for the class
     encoder = LabelEncoder()
     encoder.fit(matrix[:, -1])
     # transfrom y_train and y_test to numeric
-    y_train, y_test, y_validation = encoder.transform(y_train), encoder.transform(y_test), encoder.transform(y_validation)
+    y_train, y_test = encoder.transform(y_train), encoder.transform(y_test)
     
     # create model
     model=create_model(hyperparameters)
     # train model
-    history = model.fit(x_train, y_train, epochs = hyperparameters['epochs'], batch_size = hyperparameters["batch size"], validation_data = (x_validation , y_validation) )
+    history = model.fit(x_train, y_train, epochs = hyperparameters['epochs'], batch_size = hyperparameters["batch size"] )
     # evaluate model
     result = model.evaluate(x_test,y_test, batch_size = hyperparameters["batch size"] )
 
@@ -114,19 +107,9 @@ def main(args):
     print('Model evaluation => Loss: ' + str( result[0] ) + " , Accuracy: " + str( result[1]*100 ) + "%")
     
     # plot accuracy
-    fig, axes = plt.subplots(2, 1)
-    # plot accuracy
-    axes[0].plot(history.history["accuracy"], color="blue")
-    axes[0].plot(history.history["val_accuracy"], color="green")
-    axes[0].set_xlabel("Epoch")
-    axes[0].set_ylabel("Accuracy")
-    axes[0].legend(['train', 'test'], loc='lower right')
-    # plot loss
-    axes[1].plot(history.history["loss"], color="blue")
-    axes[1].plot(history.history["val_loss"], color="green")
-    axes[1].set_xlabel("Epoch")
-    axes[1].set_ylabel("Loss")
-    axes[1].legend(['train', 'validation'], loc='upper right')
+    plt.plot(history.history["accuracy"])
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
     # show plot
     plt.show()
 
